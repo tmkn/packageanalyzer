5ab917aa43696f9e5cc0acdefc9854586e2161c3
tmkn <tuomasklein@gmail.com>
2019-07-09 21:46:21 +0200

    wip tests for PackageAnalytics

---
+++ tests/analyzer.test.ts
@@ -0,0 +1,6 @@
+describe(`PackageAnalytics Tests`, () => {
+    it(`Check licenses`);
+    it(`Checks package with most direct dependencies`);
+    it(`Checks package that is most referred`);
+    it(`Checks for package with most versions`);
+});
