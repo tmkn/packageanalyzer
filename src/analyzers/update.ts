50bff66027c38d96e86dd55d1ef3071136fddf17
tmkn <tuomasklein@gmail.com>
2020-01-20 23:06:30 +0100

    wip

---
+++ src/analyzers/update.ts
@@ -0,0 +1,6 @@
+import * as semver from "semver";
+
+//check a package for updates todo
+export async function checkForUpdate(version: string): Promise<string> {
+    throw new Error(`not implemented`);
+}
