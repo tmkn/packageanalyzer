5624584fd9f7e449ba22c64df515b22f7f1e9ab7
tmkn <tuomasklein@gmail.com>
2020-02-05 23:14:03 +0100

    wip

--- src/analyzers/update.ts
+++ src/analyzers/update.ts
@@ -1,6 +1,22 @@
 import * as semver from "semver";
+import { PackageVersion } from "../npm";
 
 //check a package for updates todo
 export async function checkForUpdate(version: string): Promise<string> {
     throw new Error(`not implemented`);
+}
+
+interface IUpdateResult {
+    name: string;
+    version: string;
+    latest: string;
+    latestSemanticMatch: string;
+    latestBugfixMatch: string;
+}
+
+type PackageWithVersion = [string, string];
+
+interface IUpdateAnalyzer {
+    checkPackage: (module: PackageWithVersion) => Promise<IUpdateResult>;
+    checkPackages: (modules: PackageWithVersion[]) => AsyncIterableIterator<IUpdateResult>;
 }
