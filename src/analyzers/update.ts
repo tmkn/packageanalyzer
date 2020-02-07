ad6b504db6b0232f2ae155446694903981e12058
tmkn <tuomasklein@gmail.com>
2020-02-07 18:19:01 +0100

    wip

--- src/analyzers/update.ts
+++ src/analyzers/update.ts
@@ -1,22 +1,115 @@
 import * as semver from "semver";
-import { PackageVersion } from "../npm";
+import { isUnpublished, INpmPackage } from "../npm";
+import { PackageProvider } from "../providers/online";
 
-//check a package for updates todo
-export async function checkForUpdate(version: string): Promise<string> {
-    throw new Error(`not implemented`);
+interface IReleaseInfo {
+    version: string;
+    releaseDate: string;
 }
 
 interface IUpdateResult {
     name: string;
     version: string;
-    latest: string;
-    latestSemanticMatch: string;
-    latestBugfixMatch: string;
+    latestOverall: IReleaseInfo;
+    latestSemanticMatch: IReleaseInfo;
+    latestMinor: IReleaseInfo;
+    latestBugfix: IReleaseInfo;
 }
 
-type PackageWithVersion = [string, string];
+export function cleanVersion(rawVersion: string): string {
+    const { version } = semver.coerce(rawVersion) ?? {};
 
-interface IUpdateAnalyzer {
-    checkPackage: (module: PackageWithVersion) => Promise<IUpdateResult>;
-    checkPackages: (modules: PackageWithVersion[]) => AsyncIterableIterator<IUpdateResult>;
+    if (typeof version === "undefined") throw new Error(`Couldn't clean version ${rawVersion}`);
+
+    return version;
+}
+
+export function getBugfixVersionString(rawVersion: string): string {
+    return `~${cleanVersion(rawVersion)}`;
+}
+
+export function getMinorVersionString(rawVersion: string): string {
+    return `^${cleanVersion(rawVersion)}`;
+}
+
+function latestVersion(pkgInfo: INpmPackage): IReleaseInfo {
+    const latest = pkgInfo["dist-tags"].latest;
+
+    return {
+        version: latest,
+        releaseDate: pkgInfo.time[latest]
+    };
+}
+
+function getMaxSatisfyingVersion(pkgInfo: INpmPackage, version: string): string {
+    const versions: string[] = [...Object.keys(pkgInfo.versions)];
+    const maxSatisfying = semver.maxSatisfying(versions, version);
+
+    if (maxSatisfying === null) throw new Error(`Couldn't find a version match`);
+
+    return maxSatisfying;
+}
+
+function getReleaseDate(pkgInfo: INpmPackage, version: string): string {
+    const releaseDate = pkgInfo.time[version];
+
+    if (typeof releaseDate === "undefined") throw new Error(`Couldn't find the releae date`);
+
+    return releaseDate;
+}
+
+async function getNpmPackage(name: string, provider: PackageProvider): Promise<INpmPackage> {
+    const pkgInfo = await provider.getPackageInfo(name);
+
+    if (typeof pkgInfo === "undefined" || isUnpublished(pkgInfo))
+        throw new Error(`Couldn't get data`);
+
+    return pkgInfo;
+}
+
+export async function updateCheck(
+    name: string,
+    version: string,
+    provider: PackageProvider
+): Promise<IReleaseInfo> {
+    try {
+        const pkgInfo = await getNpmPackage(name, provider);
+        const maxSatisfying = getMaxSatisfyingVersion(pkgInfo, version);
+        const releaseDate = getReleaseDate(pkgInfo, maxSatisfying);
+
+        return {
+            version: maxSatisfying,
+            releaseDate: releaseDate
+        };
+    } catch (e) {
+        throw e; //todo remove try/catch
+    }
+}
+
+export async function updateInfo(
+    name: string,
+    version: string,
+    provider: PackageProvider
+): Promise<IUpdateResult> {
+    try {
+        const bugfixVersionString = getBugfixVersionString(version);
+        const minorVersionString = getMinorVersionString(version);
+
+        const pkgInfo = await getNpmPackage(name, provider);
+        const semVerUpdate: IReleaseInfo = await updateCheck(name, version, provider);
+        const bugfixUpdate: IReleaseInfo = await updateCheck(name, bugfixVersionString, provider);
+        const minorUpdate: IReleaseInfo = await updateCheck(name, minorVersionString, provider);
+        const latestUpdate: IReleaseInfo = latestVersion(pkgInfo);
+
+        return {
+            name,
+            version,
+            latestOverall: latestUpdate,
+            latestSemanticMatch: semVerUpdate,
+            latestBugfix: bugfixUpdate,
+            latestMinor: minorUpdate
+        };
+    } catch (e) {
+        throw e; //todo remove try/catch
+    }
 }
--- tests/onlineProvider.test.ts
+++ tests/onlineProvider.test.ts
@@ -1,12 +1,7 @@
-import * as path from "path";
-import * as fs from "fs";
-import { Server } from "http";
-import * as express from "express";
-
-import { INpmPackage, isUnpublished } from "../src/npm";
 import { OnlinePackageProvider } from "../src/providers/online";
 import { Visitor } from "../src/visitors/visitor";
 import { OraLogger } from "../src/logger";
+import { MockNpmServer } from "./server";
 
 describe.only(`OnlineProvider Tests`, () => {
     let server: MockNpmServer;
@@ -76,76 +71,3 @@ describe.only(`OnlineProvider Tests`, () => {
         server.close();
     });
 });
-
-class MockNpmServer {
-    private _server: Server;
-    private _dataPath = path.join("tests", "data", "mockserverdata");
-    private _cache: Map<string, INpmPackage> = new Map();
-
-    constructor(private readonly _port: number) {
-        const app = express();
-
-        this._populateCache();
-
-        app.get(`/:name/:version`, (req, res) => {
-            const { name, version } = req.params;
-            const data = this._cache.get(name);
-
-            if (typeof data === "undefined") {
-                res.status(404).send("Not found");
-            } else {
-                const versionData = data.versions[version];
-
-                if (typeof versionData === "undefined") {
-                    res.status(404).send(`version not found: ${version}`);
-                } else {
-                    res.json(versionData);
-                }
-            }
-        });
-
-        app.get(`/:name`, (req, res) => {
-            const { name } = req.params;
-
-            if (name === "unpublished") {
-                const data = {
-                    name: "unpublished",
-                    time: {
-                        unpublished: {
-                            maintainers: [],
-                            name: isUnpublished
-                        }
-                    }
-                };
-
-                res.json(data);
-            } else {
-                const data = this._cache.get(name);
-
-                if (typeof data === "undefined") {
-                    res.status(404).send({ error: "Not found" });
-                } else {
-                    res.json(data);
-                }
-            }
-        });
-
-        this._server = app.listen(this._port, () => console.log(`Started MockNpmServer`));
-    }
-
-    private _populateCache(): void {
-        const files = fs.readdirSync(this._dataPath).map(f => path.join(this._dataPath, f));
-
-        for (const file of files) {
-            const name = path.basename(file, `.json`);
-
-            this._cache.set(name, JSON.parse(fs.readFileSync(file, "utf8")));
-        }
-    }
-
-    close(): void {
-        this._server.close(e => {
-            if (e) console.log(e);
-        });
-    }
-}
