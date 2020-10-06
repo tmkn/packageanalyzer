a8f8c57ab39e6fbb16597f8558430e6935132b2a
tmkn <tuomasklein@gmail.com>
2020-10-06 23:09:29 +0200

    restructured cli files

---
+++ src/cli/common.ts
@@ -0,0 +1,129 @@
+import * as path from "path";
+import * as fs from "fs";
+
+import * as dayjs from "dayjs";
+
+import { PackageAnalytics, VersionSummary, GroupedLicenseSummary } from "../analyzers/package";
+
+export function getVersion(): string {
+    try {
+        const file = path.join(__dirname, "./../../package.json");
+
+        return JSON.parse(fs.readFileSync(file, "utf8")).version;
+    } catch (e) {
+        return "version parse error!";
+    }
+}
+
+export function daysAgo(date: string | number | Date): string {
+    return `(${dayjs(new Date()).diff(date, "day")} days ago)`;
+}
+
+export function printStatistics(pa: PackageAnalytics): void {
+    const padding = 40;
+    const paddingLeft = 4;
+
+    console.log(`=== Statistics for ${pa.fullName} ===\n`);
+
+    printPublished(pa, padding);
+    printOldest(pa.oldest, padding);
+    printNewest(pa.newest, padding);
+    console.log(`${`Direct dependencies:`.padEnd(padding)}${pa.directDependencyCount}`);
+    console.log(`${`Transitive dependencies:`.padEnd(padding)}${pa.transitiveDependenciesCount}`);
+    printDistinctDependencies(pa.distinctByNameCount, pa.distinctByVersionCount, paddingLeft);
+    printMostReferred(pa.mostReferred, padding);
+    printMostDependencies(pa.mostDependencies, padding);
+    printMostVersion(pa.mostVersions, padding, paddingLeft);
+    printLoops(pa, padding, paddingLeft);
+    printLicenseInfo(pa.licensesByGroup, paddingLeft);
+}
+
+function printNewest(newest: PackageAnalytics | undefined, padding: number): void {
+    if (newest && newest.published) {
+        console.log(
+            `${`Newest package:`.padEnd(padding)}${
+                newest.fullName
+            } - ${newest.published.toUTCString()} ${daysAgo(newest.published)}`
+        );
+        console.log(`${`Newest package path:`.padEnd(padding)}${newest.pathString}`);
+    }
+}
+
+function printOldest(oldest: PackageAnalytics | undefined, padding: number): void {
+    if (oldest && oldest.published) {
+        console.log(
+            `${`Oldest package:`.padEnd(padding)}${
+                oldest.fullName
+            } - ${oldest.published.toUTCString()} ${daysAgo(oldest.published)}`
+        );
+        console.log(`${`Oldest package path:`.padEnd(padding)}${oldest.pathString}`);
+    }
+}
+
+function printPublished(pa: PackageAnalytics, padding: number): void {
+    if (!pa.published) return;
+
+    console.log(
+        `${`Published:`.padEnd(padding)}${pa.published.toUTCString()} ${daysAgo(pa.published)}`
+    );
+}
+
+function printDistinctDependencies(
+    byName: number,
+    byNameAndVersion: number,
+    paddingLeft: number
+): void {
+    console.log(`Distinct dependencies:`);
+    console.log(`${``.padStart(paddingLeft)}${byName}: distinct name`);
+    console.log(`${``.padStart(paddingLeft)}${byNameAndVersion}: distinct name and version`);
+}
+
+function printLoops(pa: PackageAnalytics, padding: number, paddingLeft: number): void {
+    const { loops, loopPathMap, distinctLoopCount } = pa;
+
+    console.log(`${`Loops:`.padEnd(padding)}${loops.length} (${distinctLoopCount} distinct)`);
+
+    if (distinctLoopCount > 0) {
+        const [first] = loops.map(l => l.pathString).sort();
+
+        console.log(`    affected Packages: [${[...loopPathMap.keys()].join(", ")}]`);
+        console.log(`    e.g. ${first}`);
+
+        if (loops.length > 1)
+            console.log(`${``.padStart(paddingLeft)}+${loops.length - 1} additional loops`);
+    }
+}
+
+function printMostDependencies(pa: PackageAnalytics, padding: number): void {
+    console.log(
+        `${`Most direct dependencies:`.padEnd(padding)}"${pa.name}": ${pa.directDependencyCount}`
+    );
+}
+
+function printMostReferred(arg: [string, number], padding: number): void {
+    console.log(`${`Most referred package:`.padEnd(padding)}"${arg[0]}": ${arg[1]}x`);
+}
+
+function printMostVersion(mostVerions: VersionSummary, padding: number, paddingLeft: number): void {
+    console.log(`${`Package(s) with most versions:`.padEnd(padding)}`);
+
+    for (const [name, versions] of mostVerions) {
+        console.log(`${``.padStart(paddingLeft)}${name} [${[...versions].join(", ")}]`);
+    }
+}
+
+function printLicenseInfo(groupedLicenses: GroupedLicenseSummary, paddingLeft: number): void {
+    console.log(`Licenses:`);
+    for (const { license, names } of groupedLicenses) {
+        const threshold = 4;
+        const samples: string[] = names.slice(0, threshold);
+
+        if (names.length >= threshold)
+            console.log(
+                `${``.padStart(paddingLeft)}${license} - [${samples.join(", ")}, +${
+                    names.length - threshold
+                } more]`
+            );
+        else console.log(`${``.padStart(paddingLeft)}${license} - [${samples.join(", ")}]`);
+    }
+}
