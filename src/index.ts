0338718212b47cb31630631f779b8b57fbc6d321
tmkn <tuomasklein@gmail.com>
2021-03-02 23:50:36 +0100

    getPackageVersionfromString fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { Extractor } from "./utils/extractor";
 export {
     Visitor,
     PackageVersion,
-    getPackageVersionfromString as getNameAndVersion
+    getPackageVersionfromString
 } from "./visitors/visitor";
 export { Package } from "./package/package";
 export { FlatFileProvider } from "./providers/flatFile";
