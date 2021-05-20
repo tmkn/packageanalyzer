94aa091306ef0cec6e6dbb45c1af856cad51d747
tmkn <tuomasklein@gmail.com>
2021-05-20 22:12:47 +0200

    removed code smell

--- src/cli/reportCommand.ts
+++ src/cli/reportCommand.ts
@@ -1,18 +1,15 @@
-import * as path from "path";
-
 import { Command } from "clipanion";
 
 import { ReportService } from "../reports/ReportService";
 
 export class ReportCommand extends Command {
     @Command.String(`--config`, {
-        description: `todo help`
+        description: `path to the config file`
     })
     public config!: string;
 
     @Command.Path(`report`)
     async execute() {
-        //const config = require(path.join(process.cwd(), `tests`, `sampleReport.js`));
         const config = require(this.config);
         const reportService = new ReportService(config, this.context.stdout);
 
