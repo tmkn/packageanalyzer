c9a9c6d039ee579e173088b4131a3182ad9210d1
Thomas Klein <tuomasklein@gmail.com>
2021-06-03 13:01:45 +0200

    added description to report command

--- src/cli/reportCommand.ts
+++ src/cli/reportCommand.ts
@@ -8,6 +8,14 @@ export class ReportCommand extends Command {
     })
     public config!: string;
 
+    static /*override*/ usage = Command.Usage({
+        description: `run a series of checks defined in a config file`,
+        details: `
+            This command will run a series of checks defined in a config file
+        `,
+        examples: [[`Run a series of checks`, `$0 report --config ./path/to/config.js`]]
+    });
+
     @Command.Path(`report`)
     async execute() {
         const config = require(this.config);
