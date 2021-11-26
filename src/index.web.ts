73a732cbf7eb8a9951b2112c85287ab2d3f807df
Thomas Klein <tuomasklein@gmail.com>
2021-11-26 08:01:55 +0100

    test

--- src/index.web.ts
+++ src/index.web.ts
@@ -2,4 +2,4 @@ export { Visitor } from "./visitors/visitor";
 export type { PackageVersion } from "./visitors/visitor";
 export { Package } from "./package/package";
 export type { IPackageJsonProvider } from "./providers/provider";
-//export type { ILogger } from "../src/utils/logger";
+export type { ILogger } from "../src/utils/ILogger";
