affd7e95e8ee962123f135700707c3f6c70628a3
tmkn <tuomasklein@gmail.com>
2021-11-29 22:07:38 +0100

    test

--- build/src/index.web.d.ts
+++
@@ -1,6 +0,0 @@
-export { Visitor } from "./visitors/visitor";
-export type { PackageVersion } from "./visitors/visitor";
-export { Package } from "./package/package";
-export type { IPackageJsonProvider } from "./providers/provider";
-export type { ILogger } from "../src/utils/ILogger";
-//# sourceMappingURL=index.web.d.ts.map
--- build/src/web/index.js
+++ build/src/web/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Package = exports.Visitor = void 0;
-var visitor_1 = require("./visitors/visitor");
+var visitor_1 = require("../visitors/visitor");
 Object.defineProperty(exports, "Visitor", { enumerable: true, get: function () { return visitor_1.Visitor; } });
-var package_1 = require("./package/package");
+var package_1 = require("../package/package");
 Object.defineProperty(exports, "Package", { enumerable: true, get: function () { return package_1.Package; } });
-//# sourceMappingURL=index.web.js.map
+//# sourceMappingURL=index.js.map
