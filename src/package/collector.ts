b88e263a5c0ea2c7dfafa45e6d20bc46488f0e09
Thomas Klein <tmkn@users.noreply.github.com>
2023-06-04 11:59:28 +0200

    decoupled collect logic

--- src/package/collector.ts
+++ src/package/collector.ts
@@ -1,42 +1,40 @@
-import { Package } from "./package";
+type ItemGroup<I> = [I, ...I[]];
 
-type PackageGroup = [Package, ...Package[]];
-
-export type CollectorTuple<T> = [PackageGroup, T];
+export type CollectorTuple<D, I> = [ItemGroup<I>, D];
 
 export type MinLength1Array<T> = [T, ...T[]];
 
-export interface ICollectorNode<T> {
-    pkg: Package;
+export interface ICollectorNode<T, I> {
+    item: I;
     data: T;
 
-    parent: ICollectorNode<T> | null;
-    children: ICollectorNode<T>[];
+    parent: ICollectorNode<T, I> | null;
+    children: ICollectorNode<T, I>[];
 
-    flatten(): MinLength1Array<CollectorTuple<T>>;
+    flatten(): MinLength1Array<CollectorTuple<T, I>>;
 }
 
-export class CollectorNode<T> implements ICollectorNode<T> {
-    parent: ICollectorNode<T> | null = null;
-    children: ICollectorNode<T>[] = [];
+export class CollectorNode<D, I> implements ICollectorNode<D, I> {
+    parent: ICollectorNode<D, I> | null = null;
+    children: ICollectorNode<D, I>[] = [];
 
-    constructor(public data: T, public pkg: Package) {}
+    constructor(public data: D, public item: I, private _identity: (item: I) => string) {}
 
-    flatten(): MinLength1Array<CollectorTuple<T>> {
-        const entries: MinLength1Array<CollectorTuple<T>> = [[[this.pkg], this.data]];
-        const queue: ICollectorNode<T>[] = [...this.children];
+    flatten(): MinLength1Array<CollectorTuple<D, I>> {
+        const entries: MinLength1Array<CollectorTuple<D, I>> = [[[this.item], this.data]];
+        const queue: ICollectorNode<D, I>[] = [...this.children];
 
         while (queue.length > 0) {
             const node = queue.shift()!;
             // not the best runtime performance but it will do for now
             const existingGroup = entries.find(
-                ([[existingPkg]]) => existingPkg.fullName === node.pkg.fullName
+                ([[existingPkg]]) => this._identity(existingPkg) === this._identity(node.item)
             );
 
             if (existingGroup) {
-                existingGroup[0].push(node.pkg);
+                existingGroup[0].push(node.item);
             } else {
-                entries.push([[node.pkg], node.data]);
+                entries.push([[node.item], node.data]);
             }
 
             if (node.children.length > 0) {
