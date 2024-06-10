79f773411c8b11e3aba1525221bbfffa58edf587
Thomas Klein <tmkn@users.noreply.github.com>
2024-06-10 22:46:43 +0200

    wip attachments for lint checks

--- src/reports/lint/LintRule.ts
+++ src/reports/lint/LintRule.ts
@@ -1,25 +1,75 @@
 import { ZodType, z } from "zod";
 
 import type { IPackage } from "../../package/package";
+import { AttachmentData, IAttachment } from "../../attachments/Attachments";
+import { MaintainerCheck } from "./checks/MaintainerCheck";
+import { MissingDescription } from "./checks/MissingDescription";
+import { MetaFileAttachment } from "../../attachments/MetaFileAttachment";
+import { npmOnline } from "../../providers/online";
+import { satisfies } from "semver";
 
 const LintTypes = z.union([z.literal("error"), z.literal("warning")]);
 
 export type ILintTypes = z.infer<typeof LintTypes>;
 
-export interface ILintCheck<T = undefined> {
+interface ISharedLintCheckParams<T> {
     name: string;
-    check: (pkg: IPackage, params: T) => string | string[] | void;
     checkParams?: () => ZodType<T>;
 }
 
-export type LintRule<T> =
-    T extends ILintCheck<infer Params>
-        ? Params extends undefined
-            ? [ILintTypes, ILintCheck<Params>]
-            : [ILintTypes, T, Params]
-        : never;
+interface IBaseLintCheck<T = undefined> extends ISharedLintCheckParams<T> {
+    check: (pkg: IPackage, params: T) => string | string[] | void;
+}
 
-export const ZodLintRule = z.custom<LintRule<ILintCheck<any>>>(data => {
+interface ILintCheckWithAttachments<T = undefined, A extends IAttachment<string, any>[] = []>
+    extends ISharedLintCheckParams<T> {
+    check: (pkg: IPackage<AttachmentData<A>>, params: T) => string | string[] | void;
+    attachments: A;
+}
+
+export type ILintCheck<
+    T = undefined,
+    A extends IAttachment<string, any>[] | undefined = undefined
+> = A extends undefined ? IBaseLintCheck<T> : ILintCheckWithAttachments<T, Exclude<A, undefined>>;
+
+let abc: ILintCheck<{ tom: string }, [MetaFileAttachment]> = {
+    name: "test",
+    attachments: [new MetaFileAttachment(npmOnline)],
+    check: (pkg, params) => {
+        params.tom;
+        pkg.getAttachmentData("metafile");
+        // pkg.getAttachmentData("sdf");
+        return "";
+    }
+};
+
+// type LintRule<LintCheck extends ILintCheck<any, any>> =
+//     LintCheck extends ILintCheck<infer T, any>
+//         ? T extends undefined
+//             ? [ILintTypes, LintCheck]
+//             : [ILintTypes, LintCheck, T]
+//         : never;
+
+// const test: LintRule<MaintainerCheck> = ["error", new MaintainerCheck(), { authors: ["test"] }];
+
+// const test2: LintRule<typeof MissingDescription> = ["error", MissingDescription];
+
+//already works
+const test3: LintRuleTuple<{ tom: string }> = ["error", abc, { tom: "test" }];
+
+const test4: LintRuleTuple = [
+    "error",
+    {
+        name: "test",
+        check: (pkg, params) => {
+            return "";
+        }
+    }
+    // todo remove this required param, should not be needed if undefined
+    // undefined
+];
+
+export const ZodLintRule = z.custom<LintRuleTuple<any, any>>(data => {
     if (Array.isArray(data)) {
         const [type, check] = data;
 
@@ -35,6 +85,32 @@ export const ZodLintRule = z.custom<LintRule<ILintCheck<any>>>(data => {
     return false;
 });
 
-export function createRule<T>(...args: LintRule<ILintCheck<T>>): LintRule<ILintCheck<T>> {
+// infer the correct LintCheck type
+// it somehow does what it's supposed to do lol
+type LintRuleTuple<
+    P = undefined,
+    A extends IAttachment<string, any>[] | undefined = undefined
+> = A extends undefined
+    ? // no attachments where provided
+      P extends undefined
+        ? // include params if provided, otherwise exclude
+          [ILintTypes, IBaseLintCheck<P>]
+        : [ILintTypes, IBaseLintCheck<P>, P]
+    : // attachments where provided
+      P extends undefined
+      ? // include params if provided, otherwise exclude
+        [ILintTypes, ILintCheckWithAttachments<P, Exclude<A, undefined>>]
+      : [ILintTypes, ILintCheckWithAttachments<P, Exclude<A, undefined>>, P];
+
+// helper function to create a rule with inference
+export function createRule<P, A extends IAttachment<string, any>[] | undefined = undefined>(
+    ...args: LintRuleTuple<P, A>
+): LintRuleTuple<P, A> {
     return [...args];
 }
+
+export const LintFile = z.object({
+    rules: z.array(ZodLintRule)
+});
+
+export type ILintFile = z.infer<typeof LintFile>;
