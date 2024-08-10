23898a0d72a22ccf6df22ffcd1834c4f09d1f3f2
Thomas Klein <tmkn@users.noreply.github.com>
2024-08-11 00:50:59 +0200

    chore: upgraded to latest eslint (#1270)

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+    { ignores: ["**/*.js", "**/*.json", "**/*.txt"] },
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["tests/**/*.ts"],
+        rules: {
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-explicit-any": "off"
+        }
+    }
+];
