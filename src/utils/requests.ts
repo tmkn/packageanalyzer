aec43d7167731cfde2a4ed12cf2a8a510e142f8f
Thomas Klein <tmkn@users.noreply.github.com>
2025-07-18 23:12:07 +0200

    chore: simplified download retry logic (#1458)

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,73 +1,21 @@
-import * as http from "http";
-import * as https from "https";
-
 import type { Url } from "../reports/Validation.js";
 
-function download(url: Url, timeoutLimit: number): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        /**
-         * if startsWith http
-         * elseif startsWith https
-         * else reject
-         */
-        if (!url.startsWith(`http://`) && !url.startsWith(`https://`))
-            reject(new Error(`Wrong protocol: ${url}`));
-
-        const protocol = url.startsWith(`http://`) ? http : https;
-
-        protocol
-            .get(url, res => {
-                const { statusCode } = res;
-                let data = "";
-
-                if (statusCode !== 200) {
-                    reject(new Error(`Server Error '${url}'`));
-                    clearTimeout(id);
-                }
-
-                res.setEncoding("utf8");
-                res.on("data", chunk => {
-                    data += chunk;
-                });
-                res.on("end", () => {
-                    resolve(data);
-                    clearTimeout(id);
-                });
-            })
-            .on("error", () => {
-                reject(new Error());
-                clearTimeout(id);
-            })
-            .setTimeout(timeoutLimit, () => {
-                reject(new Error(`Timeout '${url}'`));
-                clearTimeout(id);
-            });
-
-        const id = setTimeout(() => {
-            reject(new Error(`Timeout 2 '${url}'`));
-            clearTimeout(id);
-        }, timeoutLimit);
-    });
+interface IDownloadOptions {
+    signal?: AbortSignal;
+    timeout?: number;
 }
 
 export async function downloadJson<T extends object>(
     url: Url,
-    timeoutLimit = 10000
+    { signal, timeout = 10_000 }: IDownloadOptions = {}
 ): Promise<T | null> {
-    const maxRetries = 4;
-    let retries = 0;
+    const timeoutSignal = AbortSignal.timeout(timeout);
+    const combinedSignal = signal ? AbortSignal.any([signal, timeoutSignal]) : timeoutSignal;
 
-    while (retries < maxRetries) {
-        try {
-            const response = await download(url, timeoutLimit);
+    const response = await fetch(url, { signal: combinedSignal });
+    const data = (await response.json()) as T;
 
-            return JSON.parse(response);
-        } catch {
-            retries++;
-
-            continue;
-        }
-    }
+    if (response.ok) return data;
 
     return null;
 }
