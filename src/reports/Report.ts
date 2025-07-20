263b1d2e8b3e2d7e70cc4999e528893ff3479113
Thomas Klein <tuomasklein@gmail.com>
2025-07-20 22:05:05 +0200

    feat: wip independent reports

--- src/reports/Report.ts
+++ src/reports/Report.ts
@@ -6,6 +6,32 @@ import { type IFormatter } from "../utils/formatter.js";
 import { type PackageVersion } from "../visitors/visitor.js";
 import { type DependencyTypes } from "./Validation.js";
 import type { AttachmentData, Attachments } from "../attachments/Attachments.js";
+import type { createTarAttachment } from "../attachments/TarAttachment.js";
+
+// todo add generics for attachments and params
+interface IReportConfig<
+    TAttachments extends Attachments = Attachments
+    // Params extends Record<string, any> = Record<string, any>
+> {
+    pkg: PackageVersion;
+    type: DependencyTypes;
+    depth: number;
+    attachments: TAttachments;
+    // todo enable later for lint usage
+    // params: Params;
+}
+
+type PackageFromConfig<T> =
+    T extends IReportConfig<infer Attachments> ? IPackage<AttachmentData<Attachments>> : never;
+
+type PackagesFromConfigs<T extends Array<IReportConfig<any>>> = {
+    [K in keyof T]: PackageFromConfig<T[K]>;
+};
+
+let reportConfig: IReportConfig<{ tar: ReturnType<typeof createTarAttachment> }>;
+let foo: PackageFromConfig<typeof reportConfig>;
+
+// const abc = foo!.getAttachmentData("tar");
 
 export interface IReportContext {
     stdoutFormatter: IFormatter;
@@ -41,6 +67,31 @@ export interface IReport<
 
     report(pkg: Args<PackageEntry, TAttachments>, context: IReportContext): Promise<number | void>;
     validate?(): ZodValidateObject;
+}
+
+interface IReport2<ReportConfigs extends IReportConfig[], ZodValidateObject extends z.ZodTypeAny> {
+    readonly name: string;
+    readonly configs: ReportConfigs;
+    readonly provider?: IPackageJsonProvider;
+
+    exitCode: number;
+
+    report(
+        packages: PackagesFromConfigs<ReportConfigs>,
+        context: IReportContext
+    ): Promise<number | void>;
+
+    validate?(): ZodValidateObject;
+}
+
+let foo2: IReport2<
+    [typeof reportConfig, IReportConfig<{ asdfsadf: ReturnType<typeof createTarAttachment> }>],
+    z.ZodTypeAny
+>;
+
+function test([[a, b], context]: Parameters<(typeof foo2)["report"]>) {
+    const test1 = a.getAttachmentData("tar");
+    const test2 = b.getAttachmentData("asdfsadf");
 }
 
 export type GenericReport = IReport<EntryTypes, any, z.ZodTypeAny>;
